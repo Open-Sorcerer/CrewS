8e3e136aa3b45d3b4208ed2af9358773c3952bae
Saviour1001 <harshghodkar@gmail.com>
2022-12-04 03:01:44 +0530

    added hooks

--- hooks/useSmartContract.js
+++ hooks/useSmartContract.js
@@ -1,24 +1,51 @@
-import { useContractRead } from "wagmi";
-import SplitFactoryABI from "../constants/abi/splitFactory.json";
+import { useContractRead, useAccount } from "wagmi";
+import DataDaoFactoryABI from "../constants/abi/DataDaoFactory.json";
+import DataDaoABI from "../constants/abi/DataDao.json";
+import Contracts from "../constants/contracts";
+import { ethers } from "ethers";
 
-const address = "0x72DB6ce47C5C9BAb34041672E75c5728c9a078B9";
+const dataDaoFactoryAddress = Contracts.DataDAOFactory;
 const chainID = 31415;
 
-export default function useSmartContract(
-  splitterAddresses, // send in an array of splitter addresses
-  splitterShares // send in the shares in percentage
-) {
-  const {
-    data: splitFactory,
-    isError,
-    isLoading,
-    error,
-  } = useContractRead({
-    addressOrName: address,
-    contractInterface: SplitFactoryABI,
-    functionName: "createSplit",
-    args: [splitterAddresses, splitterShares],
-    chainId: chainID,
-    watch: true,
-  });
-}
+export const useSmartContract = () => {
+  const provider = new ethers.providers.JsonRpcProvider(
+    "https://wallaby.node.glif.io/rpc/v0"
+  );
+
+  var privateKey =
+    "dede2bb74dfc0048cc820fe667a88245c3602894c10b373f32bcd02c9e1322b0";
+  var wallet = new ethers.Wallet(privateKey, provider);
+
+  const dataDaoFactoryContract = new ethers.Contract(
+    dataDaoFactoryAddress,
+    DataDaoFactoryABI,
+    wallet
+  );
+
+  const { address } = useAccount();
+
+  const getAddress = () => {
+    console.log(address);
+    return address;
+  };
+
+  const createDataDao = async () => {
+    console.log("creating your data dao");
+    const tx = await dataDaoFactoryContract.createDataDao(address);
+    console.log(tx);
+    return tx;
+  };
+
+  const getContractBalance = async () => {
+    console.log("fetching balance");
+    const balance = await dataDaoFactoryContract.getContractBalance();
+    console.log(parseInt(balance._hex, 16));
+    return balance;
+  };
+
+  return {
+    getAddress,
+    createDataDao,
+    getContractBalance,
+  };
+};
