a39c397c71f4f9e1480ce1f7a23b28028e8213e3
AAYUSH-GUPTA-coder <60979345+AAYUSH-GUPTA-coder@users.noreply.github.com>
2022-12-04 03:16:33 +0530

    Update dataDoaFactory

--- constants/contracts.js
+++ constants/contracts.js
@@ -1,5 +1,6 @@
 const Contracts = {
-  DataDAOFactory: "0x48Efc5Caa73E5B2716eb27Dc0b537B8600177a79",
+  // DataDAOFactory: "0x48Efc5Caa73E5B2716eb27Dc0b537B8600177a79",
+  DataDAOFactory: "0x62c5bBf1f4fE1e3Aa5B2117D1d767C8c75eff39c",
 };
 
 export default Contracts;
